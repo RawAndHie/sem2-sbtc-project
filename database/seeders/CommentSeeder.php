58887e4bd5d678c45f7d9e9f21f3622cd2a4fdb2
RawAndHie <ductath2008041@fpt.edu.vn>
2021-08-22 19:31:25 +0700

    Add comment seeder

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        \Illuminate\Support\Facades\DB::table('comments')->truncate();
+        \Illuminate\Support\Facades\DB::table('comments')->insert([
+            [
+                'comment_id' => 1,
+                'trade_id' => 1,
+                'account_id' => 5,
+                'content' => 'Ông rảnh lúc mấy giờ ?',
+                'create_at' => Carbon::create(2021, 8, 21, 21, 40, 16)->diffForHumans(Carbon::now()),
+                'update_at' => Carbon::now()
+            ],
+            [
+                'comment_id' => 2,
+                'trade_id' => 3,
+                'account_id' => 5,
+                'content' => 'Đổi đồ khác được không bạn ?',
+                'create_at' => Carbon::create(2021, 8, 21, 17, 40, 16)->diffForHumans(Carbon::now()),
+                'update_at' => Carbon::now()
+            ],
+            [
+                'comment_id' => 3,
+                'trade_id' => 4,
+                'account_id' => 5,
+                'content' => 'Ông rảnh lúc mấy giờ ?',
+                'create_at' => Carbon::create(2021, 8, 21, 15, 40, 16)->diffForHumans(Carbon::now()),
+                'update_at' => Carbon::now()
+            ],
+            [
+                'comment_id' => 4,
+                'trade_id' => 2,
+                'account_id' => 5,
+                'content' => 'Ông rảnh lúc mấy giờ ?',
+                'create_at' => Carbon::create(2021, 8, 22, 21, 40, 16)->diffForHumans(Carbon::now()),
+                'update_at' => Carbon::now()
+            ],
+            [
+                'comment_id' => 5,
+                'trade_id' => 1,
+                'account_id' => 5,
+                'content' => 'Ông rảnh lúc mấy giờ ?',
+                'create_at' => Carbon::create(2021, 8, 21, 11, 40, 16)->diffForHumans(Carbon::now()),
+                'update_at' => Carbon::now()
+            ]
+        ]);
+    }
+}
