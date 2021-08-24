4144919c67f317ba514d8963e4bf4f87291321e6
RawAndHie <ductath2008041@fpt.edu.vn>
2021-08-24 10:14:46 +0700

    Update PostPending page, pagitanion for index

--- resources/views/client/layout/style.blade.php
+++ resources/views/client/layout/style.blade.php
@@ -1,12 +1,13 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>RaoVat</title>
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/nice-select.css">
-<link rel="stylesheet" href="css/slick.css">
-<link rel="stylesheet" href="css/slick-theme.css">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/reponsive.css">
-<link rel="stylesheet" href="css/css.min.css">
+<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
+<link rel="stylesheet" href="{{asset('css/slick.css')}}">
+<link rel="stylesheet" href="{{asset('css/slick-theme.css')}}">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/reponsive.css')}}">
+<link rel="stylesheet" href="{{asset('css/css.min.css')}}">
+
 
 
