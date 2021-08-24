5acfac23c357ae0312fc1eb4e3351c4c807bc4c9
Raw <72859318+RawAndHie@users.noreply.github.com>
2021-08-24 16:59:03 +0700

    Update script.blade.php

--- resources/views/client/layout/script.blade.php
+++ resources/views/client/layout/script.blade.php
@@ -1,9 +1,9 @@
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/all.min.js"></script>
-<script src="js/jquery.nice-select.min.js"></script>
-<script src="js/slick.min.js"></script>
-<script src="js/main.js"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/all.min.js')}}"></script>
+<script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
+<script src="{{asset('js/slick.min.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
 <script src="{{URL::asset('js/alert-error.js')}}"></script>
 {{--<script src="{{URL::asset('js/raovat.min.js')}}"></script>--}}
 <script src="{{URL::asset('js/ckeditor.js')}}"></script>
