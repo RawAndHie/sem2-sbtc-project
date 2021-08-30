e483ba9c77fb54ee1deeefa82e446d57410815b6
RawAndHie <ductath2008041@fpt.edu.vn>
2021-08-30 07:06:15 +0700

    Status color model

--- app/Models/Trade.php
+++ app/Models/Trade.php
@@ -24,6 +24,7 @@ class Trade extends Model
         return $array_image;
     }
 
+
     public function getFirstImgAttribute(){
         $array_image = $this->listPhoto;
         if (count($array_image) >0){
@@ -31,6 +32,27 @@ class Trade extends Model
         } else {
             return 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg';
         }
+    }
+
+    public function getStatusColorAttribute(){
+
+        if($this->status_trade == 1){
+            return 'text-primary';
+        }elseif ($this->status_trade == 2) {
+            return 'text-warning';
+        } else {
+            return 'text-success';
+        }
+    }
+    public function getStatusNameAttribute(){
+
+            if($this->status_trade == 1){
+                return 'Chưa trao đổi';
+            }elseif ($this->status_trade == 2) {
+                return 'Đang trao đổi';
+            } else {
+                return 'Đã xong';
+            }
     }
 
 }
